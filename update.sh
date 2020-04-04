831eb068b07565dea7c1607ace581e37ddf051e5
Severin Leonhardt <serviushack@gmx.de>
2020-04-04 11:45:48 +0200

    Replace Drone CI with GitHub Actions
    The cron feature of Drone CI is disabled on cloud.drone.io[1]. Use
    GitHub Actions instead.
    
    [1] https://discourse.drone.io/t/cron-on-cloud-drone-io/3899/3

--- update.sh
+++ update.sh
@@ -4,21 +4,23 @@ set -e
 
 git remote add upstream https://github.com/mickael-kerjean/filestash.git
 git fetch upstream
-git fetch origin refs/heads/master:
 
 tags=$(curl 'https://hub.docker.com/v2/repositories/machines/filestash/tags?page_size=10')
 
 latest_digest=$(echo $tags | jq '.results[] | select(.name == "latest") | .images[0].digest')
 short_hash=$(echo $tags | jq -r ".results[] | select(.images[0].digest == $latest_digest and .name != \"latest\") | .name")
 long_hash=$(git rev-parse $short_hash)
-merge_base=$(git merge-base origin/master $long_hash)
 
 echo "Latest upstream commit from Docker Hub: $long_hash"
 echo "Lastet origin commit: $(git rev-parse origin/master)"
 
+merge_base=$(git merge-base origin/master $long_hash)
+
+# Always check out master branch to push the right branch in the next workflow step.
+git checkout origin/master
+
 if [[ "$merge_base" != "$long_hash" ]]; then
   echo "Rebasing from $merge_base to $long_hash"
-  git checkout origin/master
   git rebase $long_hash
 else
   echo "origin/master is based on latest upstream/master ($long_hash)"
